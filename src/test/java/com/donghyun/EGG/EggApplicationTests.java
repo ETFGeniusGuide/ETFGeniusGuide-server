7223221186099c72170844f77f094c79b1189cc8
김동현 <qhfhdls12@naver.com>
2024-08-06 20:05:32 +0900

    init

---
+++ src/test/java/com/donghyun/EGG/EggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donghyun.EGG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
