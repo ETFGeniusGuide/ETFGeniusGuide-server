bbdbf41e3aa32f41ef671c1ded63839c9ced7140
김동현 <qhfhdls12@naver.com>
2024-08-10 22:56:24 +0900

    feat: jpaAuditing 기능 설정파일 추가

---
+++ src/main/java/com/donghyun/EGG/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.donghyun.EGG.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
