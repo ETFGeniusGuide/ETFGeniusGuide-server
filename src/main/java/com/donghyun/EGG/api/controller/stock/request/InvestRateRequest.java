d27905d0b5225607bc946e888b3d30aca8cddc1d
김동현 <qhfhdls12@naver.com>
2024-08-14 22:14:33 +0900

    feat: 비율별 계산을 위한 요청 객체 생성

---
+++ src/main/java/com/donghyun/EGG/api/controller/stock/request/InvestRateRequest.java
@@ -0,0 +1,19 @@
+package com.donghyun.EGG.api.controller.stock.request;
+
+import com.donghyun.EGG.api.service.stock.dto.InvestRateDto;
+import lombok.Data;
+
+@Data
+public class InvestRateRequest {
+    private Integer ndxRate;
+    private Integer spxRate;
+    private Integer djiRate;
+
+    public InvestRateDto toInvestRateDto() {
+        return InvestRateDto.builder()
+                .ndxRate(this.ndxRate)
+                .spxRate(this.spxRate)
+                .djiRate(this.djiRate)
+                .build();
+    }
+}
